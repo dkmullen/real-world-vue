fe9e5ce8c01b0283c9567d3938fc9b67fb9524ae
Dennis Mullen <dkmullen@gmail.com>
2021-04-24 13:43:46 -0400

    First

---
+++ src/views/EventList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Event List</h1>
+    <event-card
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></event-card>
+  </div>
+</template>
+
+<script>
+import EventCard from '../components/EventCard.vue'
+import EventService from '../services/EventService'
+
+export default {
+  components: { EventCard },
+  data() {
+    return {
+      events: [],
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log('There was an error:', error.response)
+      })
+  },
+}
+</script>
+
+<style></style>
